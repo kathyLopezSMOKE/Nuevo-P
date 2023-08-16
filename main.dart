2f79f389d3cfedeafc56214d996867e6752550cf
kathyLopezSMOKE <Katherine Lopez>
2023-08-16 16:02:23 -0500

    first commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'dart:math';
+void main(){
+//teniendo el angulo 9 en grados sexagecimales
+
+double sexa=9;
+double cent,rad;
+cent=(10*sexa)/9;
+rad=(3.141516*sexa)/180;
+
+print('El angulo en grados centecimales es :$cent');
+print('El angulo en radianes es :$rad');
+
+}
