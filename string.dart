325e06ddc072608be9677ca687e19181e5387914
kathyLopezSMOKE <Katherine Lopez>
2023-08-18 06:37:53 -0500

    tercer commit

---
+++ string.dart
@@ -0,0 +1,19 @@
+void main(List<String>args){
+String texto=' mi texto';
+String textoMultilinea='''Mi texto
+es
+multilinea
+asi se
+configura''';
+
+int suma=1+2;
+String respuesta="Respuesta es:  ${5+6}";
+
+print(texto);
+print(textoMultilinea);
+print(respuesta);
+
+int edad =29;
+String nombre="Juan";
+print("Mi nombre es $nombre y tengo $edad años");
+}
