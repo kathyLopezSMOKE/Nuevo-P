764cd1553fe62ad760950bc78b852d34c4044c2a
kathyLopezSMOKE <Katherine Lopez>
2023-08-18 06:56:26 -0500

    String metodos

--- string.dart
+++ string.dart
@@ -1,4 +1,5 @@
 void main(List<String>args){
+  /*
 String texto=' mi texto';
 String textoMultilinea='''Mi texto
 es
@@ -15,5 +16,23 @@ print(respuesta);
 
 int edad =29;
 String nombre="Juan";
-print("Mi nombre es $nombre y tengo $edad años");
+print("Mi nombre es $nombre y tengo $edad años"); */
+
+
+/*
+int numero=100;
+double precio=15.50;
+String texto= numero.toString(); //para comvertir numeros en texto
+String texto1= precio.toString();
+
+print(texto);
+print(texto1); */
+
+String numbre="Juan Gabriel Lopez";
+print(numbre.isEmpty); //verifica si la variable tiene valor asignado o no
+print(numbre.length); //cuenta la cantidad de espacios ocupados por las palabras incluidos los espacios en blanco
+print(numbre.toUpperCase()); //transforma a mayusculas 
+print(numbre.toLowerCase()); //transforma toda la cadena de texto a miniscula
+print(numbre.trim());
+
 }
